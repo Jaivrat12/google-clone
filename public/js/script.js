660f4140c4a24374a828e74d591b3ec379d8571c
Jaivrat Das <jaivratdas123456@gmail.com>
2022-01-08 20:28:21 +0530

    Added Dark Mode

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,9 @@
 document.querySelectorAll('#header-btns a, .langs a').forEach((ele) => {
     ele.addEventListener('click', e => e.preventDefault());
 });
+
+// theme toggle hangler
+document.querySelector('#theme-toggle').addEventListener('change', () => {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+});
